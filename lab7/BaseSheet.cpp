8ad226a62e437452079c3c04ec709745f5813548
KorolD <98dima988@gmail.com>
2017-05-09 18:30:47 +0300

    Update BaseSheet.cpp

--- lab7/BaseSheet.cpp
+++ lab7/BaseSheet.cpp
@@ -1,14 +1,14 @@
 #include "BaseSheet.h"
-#define ABS(a) (a>=0)?(a):(-(a))  //модуль числа без include<math>
+#define ABS(a) (a >= 0)?(a):(-(a))  //модуль числа без include<math>
 
 	BaseSheet::BaseSheet(int Amount){
-		amount=Amount;
-		stacks=new ItemStack[amount];
+		amount = Amount;
+		stacks = new ItemStack[amount];
 		return;
 	}
 
 	BaseSheet::~BaseSheet(){
-		for(int i=0; i<amount; i++){
+		for(int i = 0; i < amount; i++){
 			stacks[i].~ItemStack();
 		}
 		delete stacks;
@@ -16,26 +16,26 @@
 	}
 
 	void BaseSheet::Add(int key, String data){
-		stacks[ABS(key%amount)].Add(key,data);//здесь остаток от деления бывает отрицательным
+		stacks[ABS(key % amount)].Add(key, data);//здесь остаток от деления бывает отрицательным
 		return;
 	}
 
 	String* BaseSheet::FindKey(int key){
-		struct Item* temp=stacks[ABS(key%amount)].FindKey(key);
+		struct Item* temp=stacks[ABS(key % amount)].FindKey(key);
 		if(temp){
-			return &(temp->data); //т.к. возвращаем указатель
+			return &(temp -> data); //т.к. возвращаем указатель
 		}else{
 			return NULL;
 		}
 	}
 
 	void BaseSheet::DeleteKey(int key){
-		stacks[ABS(key%amount)].DeleteKey(key);
+		stacks[ABS(key % amount)].DeleteKey(key);
 		return;
 	}
 
 	String BaseSheet::ShowNum(int num){//получаем хэш-адрес: кл1-зн1, кл2-зн2, кл3-зн3 ...
-		return (IntToStr(num)+": "+stacks[num].Show());
+		return (IntToStr(num) + ": " + stacks[num].Show());
 	}
 
 	int BaseSheet::GetLinesAmount(){
