2cee17823a57aafa61808c46d13e519d2958b560
KorolD <98dima988@gmail.com>
2017-05-09 18:41:57 +0300

    Update InheritantSheet.cpp

--- lab7/InheritantSheet.cpp
+++ lab7/InheritantSheet.cpp
@@ -3,7 +3,7 @@
 	double InheritantSheet::Average(){
 		int total_amount = GetAmountTotal();
 		double average = 0;
-		for(int i = 0; i < amount; i++){
+		for (int i = 0; i < amount; i++) {
 			average += double(stacks[i].Average() * stacks[i].GetAmount()) / total_amount;
 		}
 		return average;
@@ -11,7 +11,7 @@
 
 	int InheritantSheet::GetAmountTotal(){
 		int total_amount = 0;
-		for(int i = 0; i < amount; i++){
+		for (int i = 0; i < amount; i++) {
 			total_amount += stacks[i].GetAmount();
 		}
 		return total_amount;
@@ -19,7 +19,7 @@
 
 	int InheritantSheet::LessThanOrEqual(double average){
 		int k = 0; //счётчик подходящих элементов
-		for(int i = 0; i < amount; i++){
+		for (int i = 0; i < amount; i++) {
 			k += stacks[i].LessThanOrEq(average);
 		}
 		return k;
